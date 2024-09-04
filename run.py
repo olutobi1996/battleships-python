f86c7e102b10e066ef3af2dcf05dd1a40ff9f01d
olutobi Adebayo <olutobi1996@icloud.com>
2024-09-04 16:30:27 +0000

    initial commit, created board size

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+Player_One = []
+Computer_Player = []
+
+board = []
+for i in range (0, 5):
+    board.append(["0"]*5)
+    print(board)
+
+new_game()
