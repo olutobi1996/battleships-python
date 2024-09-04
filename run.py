42f636cfcf7eb97d3d18d336ae6e5a45b348389c
olutobi Adebayo <olutobi1996@icloud.com>
2024-09-04 16:38:08 +0000

    added boolean username function

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
 import random
 
+def get_username();
+    while True:
+        user_name = input("Enter username here: ")
+        if user_name:
+            print("Welcome to the great Battleships game, are you ready for war! {user_name}!")
+            return user_name
+        else:
+            print("please enter username here.")
+        
+
+
 Player_One = []
 Computer_Player = []
 
