9add534ef569f2fb46b35396d4dff52bdaf2cf94
olutobi Adebayo <olutobi1996@icloud.com>
2024-09-05 18:02:33 +0000

    corrected some syntax errors

--- run.py
+++ run.py
@@ -16,30 +16,31 @@ for i in range (0, 5):
 
     def print_board(board):
         for row in board:
-            print "".join(row)
+            print ("".join(row))
 
     print_board(board)
 
-    def random_row(board)
-    return randint(0, len(board)- 1)
-    def random_col(board)
-    return randint(0, len(board[0])- 1)
+    def random_row(board):
+        return randint(0, len(board)- 1)
+    def random_col(board):
+        return randint(0, len(board[0])- 1)
 
     ship_row = random_row(board)
     ship_col = random_col(board)
     guess_row = int(input("Guess_Row, Enter number between 0-5>:"))
     guess_col = int(input("Guess_Col, Enter number between 0-5>:"))
 
-    if guess_row === ship_row and guess_col === ship_col:
-    print("Congrats You Took My Ship Down!")
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congrats You Took My Ship Down!")
     else:
         print("Unlucky You missed!")
+    except ValueError:
+        ("Only Enter A Number!")
 
         if guess_row (0-5) and guess_col not in range (0-5):
             print ("You Must Enter A Number Between 1-5!")
-            except ValueError:
-            ("Only Enter A Number!")
-            
+   
+
 
 Player_One = []
 Computer_Player = []
