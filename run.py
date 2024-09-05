d63637d4b1d892bd2e5fe7366e146250ffe5a87a
olutobi Adebayo <olutobi1996@icloud.com>
2024-09-05 18:21:13 +0000

    added new game function and hit function

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import random
 
+"""
+Enter username function and welcome to battleships game
+"""
 def get_username():
     while True:
         user_name = input("Enter username here: ")
@@ -9,6 +12,9 @@ def get_username():
         else:
             print("please enter username here.")
 
+"""
+Board Function
+"""
 board = []
 for i in range (0, 5):
     board.append(["0"]*5)
@@ -25,10 +31,15 @@ for i in range (0, 5):
     def random_col(board):
         return randint(0, len(board[0])- 1)
 
+    """
+    Guess row and column function
+     """
     ship_row = random_row(board)
     ship_col = random_col(board)
     guess_row = int(input("Guess_Row, Enter number between 0-5>:"))
     guess_col = int(input("Guess_Col, Enter number between 0-5>:"))
+
+    def play_game():
 
     if guess_row == ship_row and guess_col == ship_col:
         print("Congrats You Took My Ship Down!")
@@ -39,8 +50,16 @@ for i in range (0, 5):
 
         if guess_row (0-5) and guess_col not in range (0-5):
             print ("You Must Enter A Number Between 1-5!")
-   
-
+            if: board [row] == "X" or board [col] == "X":
+                print("You Have Already Hit This Ship!")
+                continue
+            elif: (row, column) == guess_col or (row, column) == guess_row:
+            print("Bang, What A Hit! You Smashed My Ship")
+            game_board[row][column] = "X"
+            ships_left -= 1
+            if ships_left == 0:
+                print("Congrats, you won!")
+                new_game()
 
 Player_One = []
 Computer_Player = []
