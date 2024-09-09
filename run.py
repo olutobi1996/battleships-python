f8efe2b1b1874ff553415a0cde0bf2080864cd8e
olutobi Adebayo <olutobi1996@icloud.com>
2024-09-09 19:22:11 +0000

    added ships and guesses to playgame loop

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ Player_One = []
 Computer_Player = []
 board = []
 
-"""Board Function
+"""
+Board Function
 """
 for i in range (0, 5):
     board.append(["0"]*5)
@@ -15,37 +16,30 @@ def random_row(board):
 def random_col(board):
     return random.randint(0, len(board[0])- 1)
 
+"""
+Guess row and column function
+"""
+ship_row = random_row(board)
+ship_col = random_col(board)
 
+"""
+Enter username function and welcome to battleships game
+"""
 def get_username():
     while True:
         user_name = input("Enter username here: ")
         if user_name:
-            print("Welcome to the great Battleships game, are you ready for war! {user_name}!")
+            print(f"Welcome to the great Battleships game, are you ready for war! {user_name}!")
             return user_name
         else:
             print("please enter username here.")
 
-"""
-Board Function
-"""
-board = []
-for i in range (0, 5):
-    board.append(["0"]*5)
-print(board)
+username = get_username()           
 
 def print_board(board):
-        for row in board:
-            print ("".join(row))
+    for row in board:
+        print ("".join(row))
 
-print_board(board)
-
-"""
-    Guess row and column function
-     """
-ship_row = random_row(board)
-ship_col = random_col(board)
-guess_row = int(input("Guess_Row, Enter number between 0-5>:"))
-guess_col = int(input("Guess_Col, Enter number between 0-5>:"))
 
 def play_game():
     ships_left = 1
@@ -80,6 +74,12 @@ def play_game():
 
     print("Congrats, you won!")
 
-    
+def play_again():
+    again = str(input("Do you want to play again? (type yes or no): "))
+    if again == "yes":
+        print("again")
+    else:
+        print("Goodbye My Lover")
+        return
 
-new_game()
+play_game()
