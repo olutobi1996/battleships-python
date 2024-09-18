900ba08e681b2bbdad97841290ebbd663f08de80
olutobi Adebayo <olutobi1996@icloud.com>
2024-09-18 17:21:28 +0000

    edited create ships function and added colorama to board

--- run.py
+++ run.py
@@ -2,49 +2,10 @@ import random
 import colorama
 from colorama import Fore, Back, Style
 
-colorama.init()
-
-"""
-Board Variable and Shots_Taken variable
-"""
-board = []
-shots_taken = 1
-
-"""
-Board Function
-"""
-for i in range(0, 5):
-    board.append(["0"] * 5)
-"""
-New Board Function
-"""
-def new_board():
-    board = []
-    for i in range(0, 5):
-        board.append(["0"] * 5)
-
-"""
-Ships Positions
-Ship 1
-"""
-def random_row(board):
-    return random.randint(0, len(board) - 1)
-
-
-def random_col(board):
-    return random.randint(0, len(board[0]) - 1)
-
-
-"""
-Ship Row/Col varible for ship to be hit at random placements
-"""
-ship_row = random_row(board)
-ship_col = random_col(board)
 
 """
 Enter username function and welcome to battleships game
 """
-
 def get_username():
     while True:
         user_name = input("Enter username here: ")
@@ -59,28 +20,38 @@ def get_username():
 
 username = get_username()
 
+"""
+New Board Function
+"""
+def new_board():
+    board = []
+    for i in range(0, 5):
+        board.append(["0"] * 5)
+    return board
+
 def print_board(board):
     for row in board:
         print(Back.GREEN + "".join([col if col != "S" else "0" for col in row]))
 
-def create_ships():
+def create_ships(board):
     ships = 0
     while ships < 4:
-        random_row = random.randint(0, len(board[0]) - 1)
+        random_row = random.randint(0, len(board) - 1)
         random_col = random.randint(0, len(board[0]) - 1)
-        if board[random_row][random_col] == "".join:
+        if board[random_row][random_col] == "0":
             board[random_row][random_col] = 'S'
             ships +=1
 
 """
-Play Game function while loop including 
+Play Game function while loop including
 board range, how many ships left, if user has
 hit a ship and print statment when they have won
 """
 def play_game():
-    ships_left = 1
-    new_board()
-    create_ships()
+    ships_left = 4
+    shots_taken = 0
+    board = new_board()
+    create_ships(board)
     while ships_left > 0:
         print_board(board)
         while True:
@@ -88,37 +59,40 @@ def play_game():
                 guess_row = int(input("Guess_Row, Enter number between 0-4>:"))
                 guess_col = int(input("Guess_Col, Enter number between 0-4>:"))
             except ValueError:
-                print("Invalid Number")
+                print(Fore.RED + "Invalid Number")
                 continue
             if guess_row not in range(0, 5) and guess_col not in range(0, 5):
-                print("You Must Enter A Number Between 0-4!")
+                print(Fore.RED + "You Must Enter A Number Between 0-4!")
                 continue
             elif board[guess_row][guess_col] == "X":
-                print("\033[31m" + "You Have Already Hit This Ship!")
+                print(Fore.RED + "You Have Already Hit This Ship!")
                 continue
-            elif board[guess_row][guess_col] == "O":
-                print("You Have Already Hit This Spot!")
+            elif board[guess_row][guess_col] == "M":
+                print(Fore.RED + "You Have Already Hit This Spot!")
                 continue
             break
-        if ship_row == guess_row and ship_col == guess_col:
+        if board[guess_row][guess_col] == "S":
             print("Bang, What A Hit! You Smashed My Ship")
             board[guess_row][guess_col] = "X"
             ships_left -= 1
         else:
-            print("Unlucky You missed!")
-            board[guess_row][guess_col] = "P"
-    print("Congrats, you won!")
+            print(Fore.RED + "Unlucky You missed!")
+            board[guess_row][guess_col] = "M"
+        shots_taken += 1
+
+    print_board(board)
+    print(f"Congrats, you won! You needed {shots_taken} shots")
 
 """
-Play Again function, including loop 
-string for user to answer if they 
+Play Again function, including loop
+string for user to answer if they
 would like to play again
 """
 def play_again():
     while True:
         play_game()
         restart = input("do you want to restart Y/N?")
-        if restart.lower() == "N":
+        if restart.lower() == "n":
             print("Goodbye My Lover")
             return
 play_again()
